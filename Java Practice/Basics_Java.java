aef85218de1e8f8c556d734f3dd77c04896a82f2
Vaibhav Mule <vmule942@gmail.com>
2024-09-29 21:56:43 +0530

    Initial commit

---
+++ Java Practice/Basics_Java.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Basics_Java{
+    public static void main(String args[]){
+       Scanner sc = new Scanner (System.in);
+       int a = sc.nextInt();
+       int b = sc.nextInt();
+      sc.close();
+       
+       int sum =a+b;
+       System.out.println("Sum of a & b is :"+sum);
+    }
+}
